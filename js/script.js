c41db7980baa08d5159f77a64f8a061df8d6efea
GiandoSaba <giandosaba997@gmail.com>
2021-12-03 16:58:28 +0100

    corretto

--- js/script.js
+++ js/script.js
@@ -7,9 +7,9 @@ function getRandomIntInclusive(min, max) {
 function createRandomArray(num){
     const arrayRandom = [];
     for (let i = 0; i < num; i++) {
-        let numberRandom = getRandomIntInclusive(1,20);
+        let numberRandom = getRandomIntInclusive(1,50);
         while (arrayRandom.includes(numberRandom)) {
-            numberRandom = getRandomIntInclusive(1, 20);
+            numberRandom = getRandomIntInclusive(1, 50);
         }
         arrayRandom.push(numberRandom);
     }
@@ -44,49 +44,53 @@ let seconds = 30;
 const randomNumbers = createRandomArray(5);
 const userNumbers = [];
 const main = document.querySelector('main');
-const second = document.getElementById('seconds');
-
+const randomList = createRandomArray();
 main.append(createListDom(randomNumbers));
-second.innerText = seconds;
 
-const timer = setInterval(() => {
-    if (seconds > 0) {
-        second.innerText = seconds;
-        seconds--;
-    } else {
-        second.innerText = '0'
-        clearInterval(timer);
-    }
-}, 1000);
-
-// Da lì parte un timer di 30 secondi.
 setTimeout(() => {
     main.innerHTML = '';
     
-    // Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+    const countdown = `<h1>Tempo rimanente: <span id="seconds"></span></h1>`;
+    main.innerHTML = countdown;
+    const second = document.getElementById('seconds');
+    second.innerText = seconds;
     
+    const timer = setInterval(() => {
+        if (seconds > 0) {
+            second.innerText = seconds.toFixed(1);
+            seconds -= 0.01;
+        } else {
+            second.innerText = '0'
+            clearInterval(timer);
+            declareNumberFunction();  
+        }
+    }, 10);
+}, 5000);
+
+function declareNumberFunction() {
+    main.innerHTML = '';
     alert('Inserisci i numeri visualizzati uno per volta')
     let point = 0;
     for (let i = 0; i < randomNumbers.length; i++) {
 
         let userNumber = parseInt(prompt('Inserisci un numero'));
-        
+
         while (isNaN(userNumber)) {
             userNumber = parseInt(prompt('Valore non valido. Inserisci un numero'));
         }
-        
+
         userNumbers.push(userNumber);
         if (randomNumbers.includes(userNumber)) {
             point++;
         }
     }
-    
+
     let title = '';
-    
+
     // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
     if (point == randomNumbers.length) {
         title = `<h1>Hai inserito correttamente tutti i numeri! Complimenti</h1>`;
-    } else  {
+    } else {
         title = `<h1>Hai inserito correttamente ${point} numeri</h1>`;
     }
 
@@ -94,17 +98,14 @@ setTimeout(() => {
     main.append(createListDom(randomNumbers));
     main.append(createListDom(userNumbers));
 
-    const userList = document.querySelectorAll('.list-user li');
-    const randomList = document.querySelectorAll('.list-random li');
-    
-    console.log(userList);
-    for (let i = 0; i < randomList.length; i++) {
-        const number = parseInt(userList[i].innerText);
-        if(randomNumbers.includes(number)){
-            userList[i].classList.add('green');
-        } 
-        
+    const userCreatedList = document.querySelectorAll('.list-user li');
+    const randomCreatedList = document.querySelectorAll('.list-random li');
+
+    for (let i = 0; i < randomCreatedList.length; i++) {
+        const number = parseInt(userCreatedList[i].innerText);
+        if (randomNumbers.includes(number)) {
+            userCreatedList[i].classList.add('green');
+        }
+
     }
-    
-    
-}, ((seconds+2)*1000));
+}
